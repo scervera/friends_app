7e7bb990a117aa88e9daacba84d52eaf5031cf49
Steve Cervera <cervera1@mac.com>
2020-05-28 15:58:07 -0400

    completed search user capability

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
+  # def index
+  # 	@users = User.all_except(current_user)
+  # 	@users = @users.all_friends_except(current_user)
+  # 	@friendships = Friendship.where(user_id: current_user.id)
+  # end
+
   def index
-  	@users = User.all_except(current_user)
-  	@users = @users.all_friends_except(current_user)
+  	@filtered_users = User.all_except(current_user)
+    @filtered_users = @filtered_users.all_friends_except(current_user)
   	@friendships = Friendship.where(user_id: current_user.id)
+    if params[:search]
+      search_term = params[:search].downcase.gsub(/\s+/, "")
+      @users = @filtered_users.select { |user|
+              user.email.include?(search_term)
+              # add aditional search terms here. make sure to add OR statement with ||
+            }
+    else
+      @users = []
+    end
   end
 end
